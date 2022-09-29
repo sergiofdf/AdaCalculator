eea013f9b0acdc77bb08b15026feae9d085945d3
Sérgio Fleury Dias Filho <sergiofdf@gmail.com>
2022-09-29 14:52:06 -0300

    Add project files.

---
+++ AdaCalculator.Tests/CalculatorTests.cs
@@ -0,0 +1,43 @@
+using FluentAssertions;
+
+namespace AdaCalculator.Tests
+{
+    public class CalculatorTests
+    {
+        [Theory]
+        [ClassData(typeof(TupleDataSum1))]
+        [ClassData(typeof(TupleDataSum2))]
+        [ClassData(typeof(TupleDataSum3))]
+        [ClassData(typeof(TupleDataSubtract1))]
+        [ClassData(typeof(TupleDataSubtract2))]
+        [ClassData(typeof(TupleDataSubtract3))]
+        [ClassData(typeof(TupleDataMultiply1))]
+        [ClassData(typeof(TupleDataMultiply2))]
+        [ClassData(typeof(TupleDataMultiply3))]
+        [ClassData(typeof(TupleDataDivide1))]
+        [ClassData(typeof(TupleDataDivide2))]
+        [ClassData(typeof(TupleDataDivide3))]
+        public void Calculate_DifferentOperations_ShouldReturnCorrectResult(Tuple<string, double, double> input, Tuple<string, double> output)
+        {
+            var expected = output.ToValueTuple();
+            var sut = new Calculator();
+
+            var result = sut.Calculate(input.Item1, input.Item2, input.Item3);
+
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void Calculate_DivisionByZero_ShouldThrowArgumentException()
+        {
+            var expected = ("divide", double.PositiveInfinity);
+            var sut = new Calculator();
+
+            var result = sut.Calculate("divide", 6, 0);
+
+            result.Should().BeEquivalentTo(expected);
+        }
+
+
+    }
+}
---
+++ AdaCalculator.Tests/TupleData.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+
+namespace AdaCalculator.Tests
+{
+    public class TupleDataSum1 : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] {
+                new Tuple<string, double, double>("sum", 0, 3),
+                new Tuple<string, double>("sum", 3)
+            };
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+    public class TupleDataSum2 : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] {
+                new Tuple<string, double, double>("sum", 0, 0),
+                new Tuple<string, double>("sum", 0)
+            };
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+    public class TupleDataSum3 : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] {
+                new Tuple<string, double, double>("sum", -4, 10),
+                new Tuple<string, double>("sum", 6)
+            };
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+    public class TupleDataSubtract1 : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] {
+                new Tuple<string, double, double>("subtract", 5, 0),
+                new Tuple<string, double>("subtract", 5)
+            };
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+    public class TupleDataSubtract2 : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] {
+                new Tuple<string, double, double>("subtract", 5, 3),
+                new Tuple<string, double>("subtract", 2)
+            };
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+    public class TupleDataSubtract3 : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] {
+                new Tuple<string, double, double>("subtract", 5, 6),
+                new Tuple<string, double>("subtract", -1)
+            };
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+    public class TupleDataMultiply1 : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] {
+                new Tuple<string, double, double>("multiply", 1, 0),
+                new Tuple<string, double>("multiply", 0)
+            };
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+    public class TupleDataMultiply2 : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] {
+                new Tuple<string, double, double>("multiply", 2, 10),
+                new Tuple<string, double>("multiply", 20)
+            };
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+    public class TupleDataMultiply3 : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] {
+                new Tuple<string, double, double>("multiply", 5, -3),
+                new Tuple<string, double>("multiply", -15)
+            };
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+    public class TupleDataDivide1 : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] {
+                new Tuple<string, double, double>("divide", 15, 3),
+                new Tuple<string, double>("divide", 5)
+            };
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+    public class TupleDataDivide2 : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] {
+                new Tuple<string, double, double>("divide", 10, 1),
+                new Tuple<string, double>("divide", 10)
+            };
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+    public class TupleDataDivide3 : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] {
+                new Tuple<string, double, double>("divide", 6, 3),
+                new Tuple<string, double>("divide", 2)
+            };
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+
+}
